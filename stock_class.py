fa95d67c9261afc7b5085682886a965effd7fd06
JoelYatesCollege <59893600+JoelYatesCollege@users.noreply.github.com>
2020-06-24 15:15:02 -0400

    Add files via upload

--- stock_class.py
+++ stock_class.py
@@ -1,15 +1,15 @@
 class Stock:
-    def __init__(self, ticker, opening_price=0.0, current_price=0.0, previous_price=0.0, time=0.0, can_set_peak=True,
-                 can_set_valley=True, shares_bought=0.0, bought_price=0.0):
+    def __init__(self, ticker, shares_owned=0):
         self.ticker = ticker
-        self.opening_price = opening_price
-        self.current_price = current_price
-        self.previous_price = previous_price
-        self.time = time
-        self.can_set_peak = can_set_peak
-        self.can_set_valley = can_set_valley
-        self.shares_bought = shares_bought
-        self.bought_price = bought_price
+        self.opening_price = 0.0
+        self.current_price = 0.0
+        self.previous_price = 0.0
+        self.time = 0.0
+        self.can_set_peak = False
+        self.can_set_valley = False
+        self.shares_bought = 0.0
+        self.shares_owned = shares_owned
+        self.bought_price = 0.0
         self.peak = 0.0
-        self.valley = 100000.0
-        self.change = 0.01 * opening_price
+        self.valley = 0.0
+        self.change = 0.0
