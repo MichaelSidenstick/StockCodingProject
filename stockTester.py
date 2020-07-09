baeaf7455f6ad8bdfdc159fe1c4608d31cffc728
JoelYatesCollege <59893600+JoelYatesCollege@users.noreply.github.com>
2020-07-09 14:24:44 -0400

    Update stockTester.py
    added ability to do multiple stocks in same test

--- stockTester.py
+++ stockTester.py
@@ -35,133 +35,155 @@ class Stock:
 # change_sl
 # price_time
 
+# get files to run from user
+print('Give tickers of stocks to test. Give an empty line when done.')
+tickers = []
+while True:
+    booty = input('Stock Ticker: ').upper()
+    if booty == '':
+        break
+    else:
+        tickers.append(booty)
+
+print()
+net_profit = 0.0
+
 # an example of what to input would be:
 # H:\Stocks\Data\NIO_prices
 account_equity = 100000.00
-file_name = input('File name to read (exclude .txt extension): ')
+file_names = []
 file_path = 'H:\\Stocks\\Data'
-complete_file_name = os.path.join(file_path, file_name + '.txt')
-file = open(complete_file_name, "r")
-ticker = file.readline()
-stock = Stock(ticker)
-stock.change_sl = float(file.readline())
-print('stock.change_sl: ' + str(stock.change_sl))
-possible_low = 0.0
-lowest_price = 0.0
-highest_price = 0.0
-net_profit = 0.0
+for ticker in tickers:
+    complete_file_name = os.path.join(file_path, ticker + '_prices.txt')
+    file = open(complete_file_name, "r")
+    stock = Stock(ticker)
+    # i changed how i did this so the dummy gets rid of first line
+    dummy = file.readline()
+    stock.change_sl = float(file.readline())
+    print(ticker)
+    print('stock.change_sl: ' + str(stock.change_sl))
+    possible_low = 0.0
+    lowest_price = 0.0
+    highest_price = 0.0
+    stock_net = 0.0
 
-while True:
-    line = file.readline()
-    if line == '':
-        break
+    while True:
+        line = file.readline()
+        if line == '':
+            break
 
-    # Get price and time
-    separator = line.index('_')
-    stock.current_price = float(line[0:separator])
-    stock.time = int(line[(separator + 1):])
+        # Get price and time
+        separator = line.index('_')
+        stock.current_price = float(line[0:separator])
+        stock.time = int(line[(separator + 1):])
 
-    # set high and low
-    if stock.current_price < possible_low:
-        possible_low = stock.current_price
-    if stock.current_price > highest_price:
-        lowest_price = possible_low
-        highest_price = stock.current_price
+        # set high and low
+        if stock.current_price < possible_low:
+            possible_low = stock.current_price
+        if stock.current_price > highest_price:
+            lowest_price = possible_low
+            highest_price = stock.current_price
 
-    # initializes peaks and valleys to first price given
-    if stock.state == 0:
-        stock.shadow_peak = stock.current_price
-        stock.shadow_valley = stock.current_price
-        stock.old_valley = stock.current_price
-        stock.recent_valley = stock.current_price
-        stock.old_peak = stock.current_price
-        stock.recent_peak = stock.current_price
-        highest_price = stock.current_price
-        lowest_price = stock.current_price
-        possible_low = stock.current_price
-        stock.state = 1
+        # initializes peaks and valleys to first price given
+        if stock.state == 0:
+            stock.shadow_peak = stock.current_price
+            stock.shadow_valley = stock.current_price
+            stock.old_valley = stock.current_price
+            stock.recent_valley = stock.current_price
+            stock.old_peak = stock.current_price
+            stock.recent_peak = stock.current_price
+            highest_price = stock.current_price
+            lowest_price = stock.current_price
+            possible_low = stock.current_price
+            stock.state = 1
 
-    # set peaks if a valley was just set and the price has risen enough
-    if stock.can_set_peak and stock.current_price > stock.shadow_valley + stock.change_sl:
-        stock.old_peak_time = stock.recent_peak_time
-        stock.recent_peak_time = stock.shadow_peak_time
-        stock.shadow_peak_time = stock.time
-        stock.old_peak = stock.recent_peak
-        stock.recent_peak = stock.shadow_peak
-        stock.shadow_peak = stock.current_price
-        stock.can_set_peak = False
-        stock.can_set_valley = True
-        if stock.state < 7:
-            stock.state = stock.state + 1
+        # set peaks if a valley was just set and the price has risen enough
+        if stock.can_set_peak and stock.current_price > stock.shadow_valley + stock.change_sl:
+            stock.old_peak_time = stock.recent_peak_time
+            stock.recent_peak_time = stock.shadow_peak_time
+            stock.shadow_peak_time = stock.time
+            stock.old_peak = stock.recent_peak
+            stock.recent_peak = stock.shadow_peak
+            stock.shadow_peak = stock.current_price
+            stock.can_set_peak = False
+            stock.can_set_valley = True
+            if stock.state < 7:
+                stock.state = stock.state + 1
 
-    # continue to set the earliest peak if the price is rising
-    if stock.can_set_valley and stock.current_price > stock.shadow_peak:
-        stock.shadow_peak = stock.current_price
-        # print('Shadow Peak: ' + str(stock.shadow_peak))
-        stock.can_set_peak = False
-        stock.can_set_valley = True
+        # continue to set the earliest peak if the price is rising
+        if stock.can_set_valley and stock.current_price > stock.shadow_peak:
+            stock.shadow_peak = stock.current_price
+            # print('Shadow Peak: ' + str(stock.shadow_peak))
+            stock.can_set_peak = False
+            stock.can_set_valley = True
 
-    # set valleys if a peak was just set and the price has fallen enough
-    if stock.can_set_valley and stock.current_price < stock.shadow_peak - stock.change_sl:
-        stock.old_valley_time = stock.recent_valley_time
-        stock.recent_valley_time = stock.shadow_valley_time
-        stock.shadow_valley_time = stock.time
-        stock.old_valley = stock.recent_valley
-        stock.recent_valley = stock.shadow_valley
-        stock.shadow_valley = stock.current_price
-        stock.can_set_valley = False
-        stock.can_set_peak = True
-        if stock.state < 7:
-            stock.state = stock.state + 1
+        # set valleys if a peak was just set and the price has fallen enough
+        if stock.can_set_valley and stock.current_price < stock.shadow_peak - stock.change_sl:
+            stock.old_valley_time = stock.recent_valley_time
+            stock.recent_valley_time = stock.shadow_valley_time
+            stock.shadow_valley_time = stock.time
+            stock.old_valley = stock.recent_valley
+            stock.recent_valley = stock.shadow_valley
+            stock.shadow_valley = stock.current_price
+            stock.can_set_valley = False
+            stock.can_set_peak = True
+            if stock.state < 7:
+                stock.state = stock.state + 1
 
-    # continue to set the earliest valley if the price is falling
-    if stock.can_set_peak and stock.current_price < stock.shadow_valley:
-        stock.shadow_valley = stock.current_price
-        # print('Shadow valley: ' + str(stock.shadow_valley))
-        stock.can_set_valley = False
-        stock.can_set_peak = True
+        # continue to set the earliest valley if the price is falling
+        if stock.can_set_peak and stock.current_price < stock.shadow_valley:
+            stock.shadow_valley = stock.current_price
+            # print('Shadow valley: ' + str(stock.shadow_valley))
+            stock.can_set_valley = False
+            stock.can_set_peak = True
 
-    # runs when three peaks and three valleys have been recorded
-    if stock.state == 7:
-        # calculate resistance and support lines
-        rise1 = stock.recent_peak - stock.old_peak
-        run1 = stock.recent_peak_time - stock.old_peak_time
-        stock.resistanceSlope = rise1 / run1
-        rise2 = stock.recent_valley - stock.old_valley
-        run2 = stock.recent_valley_time - stock.old_valley_time
-        stock.supportSlope = rise2 / run2
-        support = stock.recent_valley + stock.supportSlope * (stock.time - stock.recent_valley_time)
-        resistance = stock.recent_peak + stock.resistanceSlope * (stock.time - stock.recent_peak_time)
+        # runs when three peaks and three valleys have been recorded
+        if stock.state == 7:
+            # calculate resistance and support lines
+            rise1 = stock.recent_peak - stock.old_peak
+            run1 = stock.recent_peak_time - stock.old_peak_time
+            stock.resistanceSlope = rise1 / run1
+            rise2 = stock.recent_valley - stock.old_valley
+            run2 = stock.recent_valley_time - stock.old_valley_time
+            stock.supportSlope = rise2 / run2
+            support = stock.recent_valley + stock.supportSlope * (stock.time - stock.recent_valley_time)
+            resistance = stock.recent_peak + stock.resistanceSlope * (stock.time - stock.recent_peak_time)
 
-        # buy if the current price is under or on the support line
-        if stock.shares_bought == 0 and stock.current_price <= support and resistance - support > 0.03:
-            stock.shares_bought = int(float(account_equity)*0.01/stock.current_price)
-            stock.bought_price = stock.current_price
-            stock.stop_loss = stock.current_price - stock.change_sl
+            # buy if the current price is under or on the support line
+            if stock.shares_bought == 0 and stock.current_price <= support and resistance - support > 0.03:
+                stock.shares_bought = int(float(account_equity)*0.01/stock.current_price)
+                stock.bought_price = stock.current_price
+                stock.stop_loss = stock.current_price - stock.change_sl
 
-        # sell if over or on resistance line
-        if stock.current_price >= resistance and stock.shares_bought > 0:
-            # print trade
-            net_of_trade = stock.shares_bought * (stock.current_price - stock.bought_price)
-            print(str(stock.bought_price) + ' -> ' + str(stock.current_price) + ' at ' + str(stock.time) + ' for net ' +
-                  net_of_trade.__format__('.2f'))
-            net_profit = net_profit + net_of_trade
-            # reset shares bought
-            stock.shares_bought = 0
+            # sell if over or on resistance line
+            if stock.current_price >= resistance and stock.shares_bought > 0:
+                # print trade
+                net_of_trade = stock.shares_bought * (stock.current_price - stock.bought_price)
+                print(str(stock.bought_price) + ' -> ' + str(stock.current_price) + ' at ' + str(stock.time) + ' for net ' +
+                      net_of_trade.__format__('.2f'))
+                net_profit = net_profit + net_of_trade
+                stock_net = stock_net + net_of_trade
+                # reset shares bought
+                stock.shares_bought = 0
 
-        # sell if price hits stop loss value calculated
-        if stock.current_price <= stock.stop_loss and stock.shares_bought > 0:
-            # print trade
-            net_of_trade = stock.shares_bought * (stock.current_price - stock.bought_price)
-            print(str(stock.bought_price) + ' -> ' + str(stock.current_price) + ' at ' + str(stock.time) + ' for net ' +
-                  net_of_trade.__format__('.2f') + ' BY STOP LOSS')
-            net_profit = net_profit + net_of_trade
-            # reset shares bought
-            stock.shares_bought = 0
+            # sell if price hits stop loss value calculated
+            if stock.current_price <= stock.stop_loss and stock.shares_bought > 0:
+                # print trade
+                net_of_trade = stock.shares_bought * (stock.current_price - stock.bought_price)
+                print(str(stock.bought_price) + ' -> ' + str(stock.current_price) + ' at ' + str(stock.time) + ' for net ' +
+                      net_of_trade.__format__('.2f') + ' BY STOP LOSS')
+                net_profit = net_profit + net_of_trade
+                stock_net = stock_net + net_of_trade
+                # reset shares bought
+                stock.shares_bought = 0
+
+    print('\nLowest price we could\'ve bought ' + ticker + ' at: ' + str(lowest_price))
+    print('Highest price we could\'ve sold ' + ticker + ' at: ' + str(highest_price))
+    print(ticker + ' profit: ' + stock_net.__format__('.2f'))
+    stock_profit = (100.0 * (account_equity + stock_net) / account_equity).__format__('.4f')
+    print(stock_profit + '%\n')
 
 # print results
-print('\nNet profit: ' + net_profit.__format__('.2f'))
+print('Net profit: ' + net_profit.__format__('.2f'))
 percent_profit = (100.0 * (account_equity + net_profit) / account_equity).__format__('.4f')
 print(percent_profit + '%')
-print('\nLowest price we could\'ve bought at: ' + str(lowest_price))
-print('Highest price we could\'ve sold at: ' + str(highest_price))
